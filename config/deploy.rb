0c448f1871fa3cbf2bbd3cbfb56f0bdfb06ea38c
Chris Van Patten <chris@vanpattenmedia.com>
2012-09-05 19:06:26 -0400

    Update deploy recipes and Gemfile.lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,31 @@
+# Load the project information
+set :project_yml_path, "./config/project.yml"
+project = YAML.load_file(fetch(:project_yml_path))
+
+# require 'vpmframe/capistrano/base'
+
 # Require multistage for local->staging->production deployment...
 require 'capistrano/ext/multistage'
 
-# Load the project information
-set :project_yml_path, "./config/project.yml"
+set :scm,                     :git
+set :git_enable_submodules,   1
+set :stages,                  ["staging", "production"]
+set :default_stage,           "staging"
+default_run_options[:pty]   = true
+ssh_options[:forward_agent] = true
+
+set :application,      project["application"]["name"]
+set :app_name,         project["application"]["name"]
+set :user,             project["application"]["deploy_user"]
+set :app_user,         project["application"]["user"]
+set :app_group,        project["application"]["group"]
+set :app_access_users, project["application"]["access_users"]
+set :app_theme,        project["application"]["theme"]
+set :repository,       project["application"]["repo"]
+set :site_domain,      project["application"]["domain"]
 
 # Load vpmframe requirements
 require 'vpmframe/erb-render'
-require 'vpmframe/capistrano/base'
 require 'vpmframe/capistrano/assets'
 require 'vpmframe/capistrano/puppet'
 require 'vpmframe/capistrano/credentials'
@@ -28,7 +47,7 @@ before "deploy:setup", "puppet:show"
 after "deploy:setup", "permissions:fix_setup_ownership"
 
 # Upload and symlink DB credentials
-before "deploy:create_symlink", "credentials:upload_db_cred", "credentials:symlink_db_cred"
+before "deploy:create_symlink", "credentials:upload_db_cred", "credentials:upload_s3_cred", "credentials:symlink_db_cred"
 
 # Compile and upload assets
 before "deploy", "assets:local_temp_clone"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,20 +1,27 @@
 # Set stage
 set :app_stage, "staging"
 
+# Load configuration YML
 set :project_yml_path,  "./config/project.yml"
 set :database_yml_path, "./config/database.yml"
+project  = YAML.load_file(fetch(:project_yml_path))
+database = YAML.load_file(fetch(:database_yml_path))
 
-project = YAML.load_file(fetch(:project_yml_path))
+# Stage-specific server options
+set :app_server, project["application"]["servers"][fetch(:app_stage)]["ip"]
+set :app_port,   project["application"]["servers"][fetch(:app_stage)]["port"]
 
-# Server options
-server project['application']['servers'][fetch(:app_stage)]['ip'], :app, :web, :db, :primary => true
-ssh_options[:port]          = project['application']['servers'][fetch(:app_stage)]['port']
-default_run_options[:pty]   = true
-ssh_options[:forward_agent] = true
+server fetch(:app_server), :app, :web, :db, :primary => true
+ssh_options[:port] = fetch(:app_port)
 
-set :application, project["application"]["name"]
-set :app_domain,  "#{app_stage}." + project['application']['domain']
+# Stage-specific application info
+set :app_domain,    "#{app_stage}." + project["application"]["domain"]
+set :deploy_to,     "/home/#{fetch(:app_user)}/#{fetch(:app_domain)}"
+set :app_deploy_to, fetch(:deploy_to)
 
-require 'vpmframe/capistrano/base'
-require 'vpmframe/capistrano/database'
-require 'vpmframe/capistrano/multistage'
+# Database info
+set :db_name,     database[fetch(:app_stage)]["name"]
+set :db_user,     database[fetch(:app_stage)]["user"]
+set :db_password, database[fetch(:app_stage)]["password"]
+set :db_host,     database[fetch(:app_stage)]["host"]
+set :db_grant_to, database[fetch(:app_stage)]["grant_to"]
