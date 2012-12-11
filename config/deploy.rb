80e1f010d7750371790c95eb84390f3da684da0b
Chris Van Patten <chris@vanpattenmedia.com>
2012-12-11 16:23:14 -0500

    Use latest codestuffs

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,7 @@ require 'vpmframe/capistrano/assets'
 require 'vpmframe/capistrano/puppet'
 require 'vpmframe/capistrano/credentials'
 require 'vpmframe/capistrano/permissions'
+require 'vpmframe/capistrano/wp-salts'
 
 # Don't do Railsy things...
 namespace :deploy do
@@ -44,13 +45,13 @@ end
 
 # Setup related tasks
 before "deploy:setup", "puppet:show"
-after "deploy:setup", "permissions:fix_setup_ownership"
+after "deploy:setup", "permissions:fix_setup_ownership", "salts:generate_wp_salts"
 
 # Upload and symlink DB credentials
-before "deploy:create_symlink", "credentials:upload_db_cred", "credentials:upload_s3_cred", "credentials:symlink_db_cred"
+before "deploy:create_symlink", "credentials:upload_db_cred", "credentials:upload_s3_cred", "credentials:symlink_db_cred", "salts:symlink_wp_salts"
 
 # Compile and upload assets
-before "deploy", "assets:local_temp_clone", "assets:compile_local_images", "assets:compile_local_js", "assets:compile_local_css"
+before "deploy", "assets:local_temp_clone", "assets:compile_local_css", "assets:compile_local_js", "assets:compile_local_images"
 before "deploy:create_symlink", "assets:upload_asset_css", "assets:upload_asset_js", "assets:upload_asset_images"
 
 # Fix ownership
