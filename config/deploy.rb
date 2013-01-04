393224a8260895c470d34f034263bc15ec7a9233
Chris Van Patten <chris@vanpattenmedia.com>
2013-01-04 15:57:40 -0500

    Clean up deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,18 +42,37 @@ namespace :deploy do
 end
 
 # Setup related tasks
-before "deploy:setup", "puppet:show"
-after "deploy:setup", "permissions:fix_setup_ownership", "salts:generate_wp_salts"
+before "deploy:setup",
+  "puppet:show"
+
+after "deploy:setup",
+  "permissions:fix_setup_ownership",
+  "salts:generate_wp_salts"
 
 # Upload and symlink DB credentials
-before "deploy:create_symlink", "credentials:upload_db_cred", "credentials:upload_s3_cred", "credentials:symlink_db_cred", "salts:symlink_wp_salts"
+before "deploy:create_symlink",
+  "credentials:upload_db_cred",
+  "credentials:upload_s3_cred",
+  "credentials:symlink_db_cred",
+  "salts:symlink_wp_salts"
 
 # Compile and upload assets
-before "deploy", "assets:local_temp_clone", "assets:compile_local_css", "assets:compile_local_js", "assets:compile_local_images"
-before "deploy:create_symlink", "assets:upload_asset_css", "assets:upload_asset_js", "assets:upload_asset_images"
+before "deploy",
+  "assets:local_temp_clone",
+  "assets:compile_local_images",
+  "assets:compile_local_css",
+  "assets:compile_local_js"
+
+before "deploy:create_symlink",
+  "assets:upload_asset_css",
+  "assets:upload_asset_js",
+  "assets:upload_asset_images"
 
 # Fix ownership
-before "deploy:restart", "permissions:fix_deploy_ownership"
+before "deploy:restart",
+  "permissions:fix_deploy_ownership"
 
 # Cleanup
-after "deploy", "deploy:cleanup", "assets:local_temp_cleanup"
+after "deploy",
+  "deploy:cleanup",
+  "assets:local_temp_cleanup"
