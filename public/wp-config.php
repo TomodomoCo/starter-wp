c83b613f418dedfa41455bb6249189800ddccaab
Chris Van Patten <chris@vanpattenmedia.com>
2013-01-10 10:21:14 -0500

    Update wp-config w/ more streamlining/changes

--- public/wp-config.php
+++ public/wp-config.php
@@ -11,89 +11,93 @@ $database = $yaml->parse( file_get_contents( dirname( __FILE__ ) . './../config/
 // ===================================================
 $urlParts = explode( '.', $_SERVER['HTTP_HOST'] );
 if ( $urlParts[0] == 'dev' ) {
-	// Local dev
-	define( 'WP_STAGE',   'dev' );
-	define( 'WP_HOME',    'http://dev.' . $project['domain'] );
-	define( 'WP_SITEURL', 'http://dev.' . $project['domain'] . '/wp' );
+	/**
+	 * DEV
+	 */
+	define( 'WP_STAGE', $urlParts[0] );
+	define( 'WP_HOME', 'http://' . WP_STAGE . '.' . $project['domain'] );
+
+	// Make "private"
+	update_option( 'blog_public', 0 );
 
 	// Show errors
 	ini_set( 'display_errors', 1 );
 	define( 'WP_DEBUG', true );
 	define( 'WP_DEBUG_DISPLAY', true );
-
-	foreach ( $database['dev'] as $db_variable => $value ) {
-		define( ( 'DB_' . strtoupper( $db_variable ) ), $value );
-	}
-	$table_prefix = DB_TBL_PREFIX;
 } elseif ( $urlParts[0] == 'staging' ) {
-	// Staging
-	define( 'WP_STAGE', 'staging' );
-	define( 'WP_HOME',    'http://staging.' . $project['domain'] );
-	define( 'WP_SITEURL', 'http://staging.' . $project['domain'] . '/wp' );
+	/**
+	 * STAGING
+	 */
+	define( 'WP_STAGE', $urlParts[0] );
+	define( 'WP_HOME', 'http://' . WP_STAGE . '.' . $project['domain'] );
 
-	// Show errors
-	ini_set( 'display_errors', 1 );
-	define( 'WP_DEBUG', true );
-	define( 'WP_DEBUG_DISPLAY', true );
-
-	foreach ( $database['staging'] as $db_variable => $value ) {
-		define( ( 'DB_' . strtoupper( $db_variable ) ), $value );
-	}
-	$table_prefix = DB_TBL_PREFIX;
-} else {
-	// Production
-	define( 'WP_STAGE', 'production' );
-	define( 'WP_HOME',    'http://www.' . $project['domain'] );
-	define( 'WP_SITEURL', 'http://www.' . $project['domain'] . '/wp' );
+	// Make "private"
+	update_option( 'blog_public', 0 );
 
 	// Hide errors
 	ini_set( 'display_errors', 0 );
 	define( 'WP_DEBUG', false );
 	define( 'WP_DEBUG_DISPLAY', false );
+} else {
+	/**
+	 * PRODUCTION
+	 */
+	define( 'WP_STAGE', 'production' );
+	define( 'WP_HOME', 'http://www.' . $project['domain'] );
 
-	foreach ( $database['production'] as $db_variable => $value ) {
-		define( ( 'DB_' . strtoupper( $db_variable ) ), $value );
-	}
-	$table_prefix = DB_TBL_PREFIX;
+	// Make public
+	update_option( 'blog_public', 1 );
+
+	// Hide errors
+	ini_set( 'display_errors', 0 );
+	define( 'WP_DEBUG', false );
+	define( 'WP_DEBUG_DISPLAY', false );
 }
 
-// ==============
-// Misc. Settings
-// ==============
+/**
+ * Path to WordPress
+ */
+define( 'WP_SITEURL', WP_HOME . '/wp' );
+
+/**
+ * Set up databases
+ */
+foreach ( $database[ WP_STAGE ] as $db_variable => $value ) {
+	define( ( 'DB_' . strtoupper( $db_variable ) ), $value );
+}
+$table_prefix = DB_TBL_PREFIX;
+
+/**
+ * Misc. Settings
+ */
 define( 'WP_POST_REVISIONS', 8 );
 
-// ========================
-// Custom Content Directory
-// ========================
+/**
+ * Custom Content Directory
+ */
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
 define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
 
-// ================================================
-// You almost certainly do not want to change these
-// ================================================
+/**
+ * You almost certainly do not want to change these
+ */
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
-// ==============================================================
-// Salts, for security
-// ==============================================================
+/**
+ * Salts, for security
+ */
 include dirname( __FILE__ ) . './../config/wp-salts.php';
 
-// ================================
-// Language
-// Leave blank for American English
-// ================================
+/**
+ * Language
+ * Leave blank for American English
+ */
 define( 'WPLANG', '' );
 
-// ===========
-// Hide errors
-// ===========
-ini_set( 'display_errors', 0 );
-define( 'WP_DEBUG_DISPLAY', false );
-
-// ===================
-// Bootstrap WordPress
-// ===================
+/**
+ * Bootstrap WordPress
+ */
 if ( !defined( 'ABSPATH' ) )
 	define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
 require_once( ABSPATH . 'wp-settings.php' );
