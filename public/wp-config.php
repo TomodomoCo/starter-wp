eee514458f30c9e1ff3f94df0acb3a1dcef7746f
Chris Van Patten <chris@vanpattenmedia.com>
2013-01-10 11:56:22 -0500

    Move debug into its own section

--- public/wp-config.php
+++ public/wp-config.php
@@ -49,12 +49,10 @@ if ( $urlParts[0] == 'dev' ) {
 	define( 'WP_HOME', 'http://' . WP_STAGE . '.' . $project['domain'] );
 
 	// Make "private"
-	update_option( 'blog_public', 0 );
+	define( 'WP_ROBOTS_PUBLIC', 0 );
 
 	// Show errors
-	ini_set( 'display_errors', 1 );
 	define( 'WP_DEBUG', true );
-	define( 'WP_DEBUG_DISPLAY', true );
 } elseif ( $urlParts[0] == 'staging' ) {
 	/**
 	 * STAGING
@@ -63,12 +61,10 @@ if ( $urlParts[0] == 'dev' ) {
 	define( 'WP_HOME', 'http://' . WP_STAGE . '.' . $project['domain'] );
 
 	// Make "private"
-	update_option( 'blog_public', 0 );
+	define( 'WP_ROBOTS_PUBLIC', 0 );
 
 	// Hide errors
-	ini_set( 'display_errors', 0 );
 	define( 'WP_DEBUG', false );
-	define( 'WP_DEBUG_DISPLAY', false );
 } else {
 	/**
 	 * PRODUCTION
@@ -77,18 +73,43 @@ if ( $urlParts[0] == 'dev' ) {
 	define( 'WP_HOME', 'http://www.' . $project['domain'] );
 
 	// Make public
-	update_option( 'blog_public', 1 );
+	define( 'WP_ROBOTS_PUBLIC', 1 );
 
 	// Hide errors
-	ini_set( 'display_errors', 0 );
 	define( 'WP_DEBUG', false );
-	define( 'WP_DEBUG_DISPLAY', false );
 }
 
 /**
  * Path to WordPress
  */
 define( 'WP_SITEURL', WP_HOME . '/wp' );
+
+/**
+ * Custom Content Directory
+ */
+define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
+define( 'WP_CONTENT_URL', WP_HOME . '/content' );
+
+/**
+ * Misc. Settings
+ */
+define( 'WP_POST_REVISIONS', 8 );
+
+/**
+ * Language (leave blank for American English)
+ */
+define( 'WPLANG', '' );
+
+/**
+ * Debug settings
+ */
+if ( WP_DEBUG == true ) {
+	ini_set( 'display_errors', '1' );
+	define( 'WP_DEBUG_DISPLAY', true );
+} else {
+	ini_set( 'display_errors', '0' );
+	define( 'WP_DEBUG_DISPLAY', false );
+}
 
 /**
  * Set up databases
@@ -98,24 +119,12 @@ if ( array_key_exists( WP_STAGE, $database ) ) {
 		define( ( 'DB_' . strtoupper( $db_variable ) ), $value );
 	}
 	$table_prefix = DB_TBL_PREFIX;
-}
-else {
+} else {
 	header('HTTP/1.1 503 Service Unavailable');
 	echo $failure_message;
 	trigger_error( 'WPFRAME FATAL: === The database.yml file does not have a \'' . strip_tags( WP_STAGE ) . '\' stage, so the DB details cannot be loaded for the ' . strip_tags( $_SERVER['SERVER_NAME'] ) . ' site.', E_USER_ERROR );
 	die();
 }
-
-/**
- * Misc. Settings
- */
-define( 'WP_POST_REVISIONS', 8 );
-
-/**
- * Custom Content Directory
- */
-define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
-define( 'WP_CONTENT_URL', 'http://' . strip_tags( $_SERVER['SERVER_NAME'] ) . '/content' );
 
 /**
  * You almost certainly do not want to change these
@@ -126,18 +135,11 @@ define( 'DB_COLLATE', '' );
 /**
  * Salts, for security
  */
-if ( @file_exists( dirname( __FILE__ ) . './../config/wp-salts.php' ) ) {
-	include dirname( __FILE__ ) . './../config/wp-salts.php';
-}
-else {
+if ( file_exists( dirname( __FILE__ ) . '/./../config/wp-salts.php' ) ) {
+	include dirname( __FILE__ ) . '/./../config/wp-salts.php';
+} else {
 	trigger_error( 'There is no config/wp-salts.php file for the ' . strip_tags( $_SERVER['SERVER_NAME'] ) . ' site.' , E_USER_WARNING );
 }
-
-/**
- * Language
- * Leave blank for American English
- */
-define( 'WPLANG', '' );
 
 /**
  * Bootstrap WordPress
@@ -145,3 +147,9 @@ define( 'WPLANG', '' );
 if ( !defined( 'ABSPATH' ) )
 	define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
 require_once( ABSPATH . 'wp-settings.php' );
+
+/**
+ * Set 'blog_public'
+ */
+if ( defined( 'WP_ROBOTS_PUBLIC' ) )
+	update_option( 'blog_public', WP_ROBOTS_PUBLIC );
